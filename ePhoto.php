fc9b26caddd8c37ddc715ebc75a0a83aaeb998b3
Einden Studio <Einden Studio@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-02-09 21:46:43 +0000

    Bugs fix
    git-svn-id: https://plugins.svn.wordpress.org/ephoto-plugin/trunk@665717 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- ePhoto.php
+++ ePhoto.php
@@ -3,7 +3,7 @@
 Plugin Name: ePhoto
 Plugin URI: http://www.ephoto.fr/
 Description: Add ePhoto connector in the TinyMCE Rich Visual Editor
-Version: 2.0.0
+Version: 2.0.2
 Author: Einden Studio
 Author URI: http://www.einden.com/
 
